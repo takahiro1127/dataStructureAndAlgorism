8d6e7f34b803e6576dcf7b21dc4861cd96bc2d9e
Uehara takahiro <takahiro.steadtler@gmail.com>
2019-07-05 02:54:55 +0900

    maximumHeap

---
+++ heap/maximumHeap.go
@@ -0,0 +1,50 @@
+package main
+
+import(
+	// "bufio"
+	"fmt"
+	// "os"
+	"strconv"
+	// "github.com/k0kubun/pp"
+	"../procon"
+)
+
+func main() {
+	_, err := strconv.Atoi(procon.StrStdin())
+	if err != nil {
+		panic(err)
+	}
+	randomArray, err := procon.SplitIntStdin(" ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(buildMaxHeap(randomArray))
+}
+
+func buildMaxHeap(A []int) []int{
+	for i := len(A)/2 - 1; i >= 0; i-- {
+		A = maxHeapify(A, i)
+	}
+	return A
+}
+
+func maxHeapify(A []int, i int) []int {
+	l := 2 * i
+	r := 2 * i + 1
+
+	var largest int
+	if l <= len(A) && A[l] > A[i] {
+		largest = l
+	} else {
+		largest = i
+	}
+	if r <= len(A) && A[r] > A[largest] {
+		largest = r
+	}
+
+	if largest != i {
+		A[i], A[largest] = A[largest], A[i]
+		maxHeapify(A, largest)
+	}
+	return A
+}
